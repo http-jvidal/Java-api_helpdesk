0d3e69dc5e85aaf52245eb5b30fe80cbd5ab3cd0
João Henrique <joaohenriquevidalribeiro@gmail.com>
2023-11-23 14:45:10 -0300

    start somethings in security for example: JWTObject, Security Config and others

--- src/main/java/api/helpdesk/security/JWTFilter.java
+++ src/main/java/api/helpdesk/security/JWTFilter.java
@@ -1,12 +1,52 @@
 package api.helpdesk.security;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
+import io.jsonwebtoken.UnsupportedJwtException;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.security.SignatureException;
+import java.util.List;
 
 public class JWTFilter extends OncePerRequestFilter{
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response)
-    
-    
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = request.getHeader(JWTCreator.HEADER_AUTORIZATION);
+
+        try{
+            if(token != null && !token.isEmpty()){
+                JWTObject tokenObject = JWTCreator.create(token,SecurityConfig.PREFIX, SecurityConfig.KEY);
+
+                List<SimpleGrantedAuthority> authorities = authorities(tokenObject.getRoles());
+
+
+                UsernamePasswordAuthenticationToken userToken =
+                        new UsernamePasswordAuthenticationToken(
+                                tokenObject.getSubject(),
+                                null,
+                                authorities);
+
+                SecurityContextHolder.getContext().setAuthentication(userToken);
+            }else{
+                SecurityContextHolder.clearContext();
+            }
+            filterChain.doFilter(request, response);
+        } catch(ExpiredJwtException | UnsupportedJwtException | MalformedJwtException | SignatureException e) {
+            e.printStackTrace();
+            response.setStatus(HttpStatus.FORBIDDEN.value());
+            return;
+        }
+
+    }
 }
